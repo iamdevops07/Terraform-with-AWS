d6977a00174df18101985edbf49ac6b34215187d
iamdevops07 <100075786+iamdevops07@users.noreply.github.com>
2022-07-03 23:14:59 +0530

    Update main.tf

--- multi-vpc-with-tgw/main.tf
+++ multi-vpc-with-tgw/main.tf
@@ -76,6 +76,9 @@ module "mgmt_vpc" {
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
+    
+       
+  create_egress_only_igw                         = true  
   enable_ipv6                                    = true
   private_subnet_assign_ipv6_address_on_creation = true
   private_subnet_ipv6_prefixes                   = [0, 1, 2]
